19098c05a931cf14d20821ea583115814bad0faf
Unabatedlynx5 <frankrobertdenton@gmail.com>
2023-02-02 18:59:57 -0600

    Photonvision

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+import org.photonvision.PhotonCamera;
+
+public final class Constants {
+    public static final class cameras{
+        public PhotonCamera kPiCam = new PhotonCamera("IMX219");
+    }
+}
