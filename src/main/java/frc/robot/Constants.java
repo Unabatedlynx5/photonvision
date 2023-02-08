bbf01c8c28fdd7a324b68436998e7b82334d6860
Unabatedlynx5 <frankrobertdenton@gmail.com>
2023-02-07 20:07:22 -0600

    working on photonvision

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,9 +1,14 @@
 package frc.robot;
 
-import org.photonvision.PhotonCamera;
 
 public final class Constants {
-    public static final class cameras{
-        public PhotonCamera kPiCam = new PhotonCamera("IMX219");
+    public static final class subsystemDebuggers{
+        public static final boolean kVisionDebug = false;
+        public static final boolean kDrivetrainDebug = false;
+        public static final boolean kClimberDebug = false;
+        public static final boolean kIcsDebug = true;
+        public static final boolean kConveyDebug = false;
+        public static final boolean kPneumaticsDebug = false;  // TODO turn this off for competition
+        public static final boolean kCameraDebug = true;
     }
 }
